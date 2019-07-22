3045301b7bd37c9659978365fde20a5a2eb6c220
mattyd2017 <32998214+mattyd2017@users.noreply.github.com>
2019-07-22 17:39:07 +0100

    Updates

---
+++ precache-manifest.04b9b2273013647271d68192f009958a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2564242be2a9e3d95b7fed25186a285a",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "106454423d29bfdcfeaa",
+    "url": "/monsters/static/css/main.954c4d26.chunk.css"
+  },
+  {
+    "revision": "6ea22432601858ce9ef5",
+    "url": "/monsters/static/js/2.27e9ad4f.chunk.js"
+  },
+  {
+    "revision": "106454423d29bfdcfeaa",
+    "url": "/monsters/static/js/main.b98b487f.chunk.js"
+  },
+  {
+    "revision": "0b202f8c0ed788dad152",
+    "url": "/monsters/static/js/runtime~main.39cb6628.js"
+  }
+]);
